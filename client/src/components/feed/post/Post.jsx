6c28becf87229d6f60e59543b42cd6920ba2f3dc
ShahmarALiyev <saliyev2019@gmail.com>
2022-05-26 17:41:42 -0500

    added like functionality with usestate

--- client/src/components/feed/post/Post.jsx
+++ client/src/components/feed/post/Post.jsx
@@ -3,8 +3,17 @@ import MoreVertIcon from "@mui/icons-material/MoreVert";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import { Users } from "../../../data";
+import { useState } from "react";
 
 function Post({ post }) {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="post">
       <div className="postContainer">
@@ -41,11 +50,15 @@ function Post({ post }) {
 
         <div className="postBottom">
           <div className="postBottomLeft">
-            <FavoriteIcon className="postReactionHeart postReaction" />
-            <ThumbUpIcon className="postReactionLIike postReaction" />
-            <span className="postReactionCounter">
-              {post.like} people reacted
-            </span>
+            <FavoriteIcon
+              className="postReactionHeart postReaction"
+              onClick={likeHandler}
+            />
+            <ThumbUpIcon
+              className="postReactionLIike postReaction"
+              onClick={likeHandler}
+            />
+            <span className="postReactionCounter">{like} people reacted</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
--- client/src/components/leftbar/closeFriends/CloseFriends.jsx
+++ client/src/components/leftbar/closeFriends/CloseFriends.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "./CloseFriends.styles.css";
 
 export default function CloseFriends({ user }) {
   return (
