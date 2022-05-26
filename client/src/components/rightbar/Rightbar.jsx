1d885fbf8f7a38d12b16f4709d2892be39555413
ShahmarALiyev <saliyev2019@gmail.com>
2022-05-26 10:42:53 -0500

    Created right bar component

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,78 @@
 import "./Rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarContainer">
+        <div className="birthdayContainer">
+          <img className="birthdayImage" alt="gift" src="assets/gift.png" />
+          <span className="birthdayText">
+            <b> Kobe Bryant </b>and <b> Lebron James </b>have a birthday today.
+          </span>
+        </div>
+
+        <img className="rightbarAd" alt="" src="/assets/ad.jpg" />
+
+        <h4 className="rightbarTitle"> Online Friends</h4>
+
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImageContainer">
+              <img
+                className="rightbarProfileImage"
+                alt=""
+                src="/assets/curry.jpg"
+              />
+              <span className="rightbarOnline"> </span>
+            </div>
+            <span className="rightbarUserName"> Stepen Curry</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImageContainer">
+              <img
+                className="rightbarProfileImage"
+                alt=""
+                src="/assets/curry.jpg"
+              />
+              <span className="rightbarOnline"> </span>
+            </div>
+            <span className="rightbarUserName"> Stepen Curry</span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImageContainer">
+              <img
+                className="rightbarProfileImage"
+                alt=""
+                src="/assets/curry.jpg"
+              />
+              <span className="rightbarOnline"> </span>
+            </div>
+            <span className="rightbarUserName"> Stepen Curry</span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImageContainer">
+              <img
+                className="rightbarProfileImage"
+                alt=""
+                src="/assets/curry.jpg"
+              />
+              <span className="rightbarOnline"> </span>
+            </div>
+            <span className="rightbarUserName"> Stepen Curry</span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImageContainer">
+              <img
+                className="rightbarProfileImage"
+                alt=""
+                src="/assets/curry.jpg"
+              />
+              <span className="rightbarOnline"> </span>
+            </div>
+            <span className="rightbarUserName"> Stepen Curry</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
