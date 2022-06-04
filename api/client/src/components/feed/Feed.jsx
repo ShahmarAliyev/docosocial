46a0bf6a828d5449cb79dce9cfa17c36da6f80a3
ShahmarALiyev <saliyev2019@gmail.com>
2022-06-04 16:18:25 -0500

    first commit

--- api/client/src/components/feed/Feed.jsx
+++ api/client/src/components/feed/Feed.jsx
@@ -22,9 +22,10 @@ export default function Feed({ username }) {
               user._id
           );
       setPosts(
-        res.data.sort((p1, p2) => {
-          return new Date(p2.createdAt) - new Date(p1.createdAt);
-        })
+        res.data
+        // .sort((p1, p2) => {
+        //   return new Date(p2.createdAt) - new Date(p1.createdAt);
+        // })
       );
     };
     fetchPosts();
